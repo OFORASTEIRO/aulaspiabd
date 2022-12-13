82fc63020f0ee4da9f349cd907646cbf2958b57f
Paulo Ricardo da Silva Matias <p.matias@academico.ifrn.edu.br>
2022-12-13 16:57:42 -0300

    colocando os arquivos inciais da minha aplicação

---
+++ src/main/java/ifrn/piabd/eventos/IndexController.java
@@ -0,0 +1,18 @@
+package ifrn.piabd.eventos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Chamou o index");
+		return "home";
+	}
+	
+}
+ 
+
+ 
